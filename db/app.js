e13247d29c4a6b0a99c7a370c96cc45fafd44945
Paulo Sabanae <100443496+psabanae@users.noreply.github.com>
2022-04-27 21:07:17 -0400

    Update app.js
    Updated security

--- db/app.js
+++ db/app.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg');
 
 const client = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://ncljbmwfdtqmip:642b6596e62d3d30de8c297bee4a003781d109e6f0cf9640b937a2b9a32429a4@ec2-3-211-6-217.compute-1.amazonaws.com:5432/dc8oqlg0puv7ek',
+    connectionString: process.env.DATABASE_URL,
     ssl:{
         rejectUnauthorized: false
     }
